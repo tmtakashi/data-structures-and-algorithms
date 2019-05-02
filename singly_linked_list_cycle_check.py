70ed6b6ddf2057d2be16079aba4395f18d1868e8
tmtakashi <tmtakshi7@gmail.com>
2019-05-02 16:08:01 +0900

    add

---
+++ singly_linked_list_cycle_check.py
@@ -0,0 +1,30 @@
+class Node(object):
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def cycle_check(node):
+    first_node = node
+    while True:
+        node = node.next
+        if node == first_node:
+            return True
+        elif node == None:
+            return False
+
+
+if __name__ == "__main__":
+    a = Node(1)
+    b = Node(2)
+    c = Node(3)
+
+    a.next = b
+    b.next = c
+
+    print(cycle_check(a))
+
+    c.next = a
+
+    print(cycle_check(a))
