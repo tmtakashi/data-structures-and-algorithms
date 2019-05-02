4c3f59ddef6a6ef24c0b6a47e820a8f36e61ff46
tmtakashi <tmtakshi7@gmail.com>
2019-05-02 15:46:26 +0900

    add

---
+++ queue_using_two_stacks.py
@@ -0,0 +1,18 @@
+class Queue2Stacks(object):
+
+    def __init__(self):
+        self.instack = []
+        self.outstack = []
+
+    def enqueue(self, element):
+        self.instack.append(element)
+
+    def dequeue(self):
+        if not self.outstack:
+            while self.instack:
+                self.outstack.append(self.instack.pop())
+        return self.outstack.pop()
+
+
+if __name__ == "__main__":
+    q = Queue2Stacks()
