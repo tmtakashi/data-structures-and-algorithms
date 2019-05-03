57cf0c6f4921c3bd9d9537e877cd98ff55c92ebc
tmtakashi <tmtakashi7@gmail.com>
2019-05-03 21:55:42 +0900

    add

---
+++ recursion/reverse_a_string.py
@@ -0,0 +1,11 @@
+def reverse(s):
+    if len(s) == 1:
+        return s
+    else:
+        last = s[-1]
+        s = s[:-1]
+        return last + reverse(s)
+
+
+if __name__ == "__main__":
+    print(reverse('hello world'))
